14b1b7bc23072e17bc0f08ff3bdac58fb87c4f30
David Sudia <dsudia@gmail.com>
2016-03-07 16:52:11 -0700

    add all existing shoes to seed file

--- seeds/1_add_products.js
+++ seeds/1_add_products.js
@@ -7,6 +7,8 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('table_name').insert({name: 'Petrol Shoe', color: 'blue', image_file: 'images/womens-petrol-shoe-blue.png', price: 49.99, gender: 'f'}),
     knex('table_name').insert({name: 'Petrol Boot', color: 'blue', image_file: 'images/womens-petrol-boot-blue.png', price: 69.99, gender: 'f'}),
-    knex('table_name').insert({name: , image_file: , price: , gender: , size: }),
+    knex('table_name').insert({name: 'Rouge Slipper', color: 'red', image_file: 'images/womens-slipper-red.png', price: 39.99, gender: 'f'}),
+    knex('table_name').insert({name: 'Rouge Boot', color: 'red', image_file: 'images/womens-boot-red.png', price: 69.99, gender: 'f'}),
+    knex('table_name').insert({name: 'Coal Boot', color: 'black', image_file: 'images/womens-boot-black.png', price: 69.99, gender: 'f'}),
   );
 };
