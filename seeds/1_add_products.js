7044ecfab65707711951f3e7ce362f02ebd8e93e
David Sudia <dsudia@gmail.com>
2016-03-07 16:57:14 -0700

    add two men’s shoes to seed file

--- seeds/1_add_products.js
+++ seeds/1_add_products.js
@@ -10,5 +10,7 @@ exports.seed = function(knex, Promise) {
     knex('table_name').insert({name: 'Rouge Slipper', color: 'red', image_file: 'images/womens-slipper-red.png', price: 39.99, gender: 'f'}),
     knex('table_name').insert({name: 'Rouge Boot', color: 'red', image_file: 'images/womens-boot-red.png', price: 69.99, gender: 'f'}),
     knex('table_name').insert({name: 'Coal Boot', color: 'black', image_file: 'images/womens-boot-black.png', price: 69.99, gender: 'f'}),
+    knex('table_name').insert({name: 'Black Oxford', color: 'black', image_file: 'images/mens-oxfords-black.png', price: 99.99, gender: 'm'}),
+    knex('table_name').insert({name: 'Hip Sneaks', color: 'blue', image_file: 'images/mens-sneaker-blue.png', price: 39.99, gender: 'm'})
   );
 };
