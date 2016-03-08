bb18113807b1a8dd5e71130c5f840e925f77602c
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-08 16:51:04 -0700

    got all products

--- queries.js
+++ queries.js
@@ -1,6 +1,12 @@
 var knex = require('./db/knex');
+function Shoes() {
+  return knex('products')
+}
 
 
 module.exports = {
+  getShoes: function() {
+    return Shoes()
+  }
 
 };
--- seeds/1_add_products.js
+++ seeds/1_add_products.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
     knex('products').insert({name: 'Rouge Slipper', color: 'red', image_file: 'images/womens-slipper-red.png', price: 39.99, gender: 'f'}),
     knex('products').insert({name: 'Rouge Boot', color: 'red', image_file: 'images/womens-boot-red.png', price: 69.99, gender: 'f'}),
     knex('products').insert({name: 'Coal Boot', color: 'black', image_file: 'images/womens-boot-black.png', price: 69.99, gender: 'f'}),
-    knex('products').insert({name: 'Black Oxford', color: 'black', image_file: 'images/mens-oxfords-black.png', price: 99.99, gender: 'm'}),
-    knex('products').insert({name: 'Hip Sneaks', color: 'blue', image_file: 'images/mens-sneaker-blue.png', price: 39.99, gender: 'm'})
+    knex('products').insert({name: 'Black Oxford', color: 'black', image_file: 'images/mens-oxfords-black.jpg', price: 99.99, gender: 'm'}),
+    knex('products').insert({name: 'Hip Sneaks', color: 'blue', image_file: 'images/mens-sneaker-blue.jpg', price: 39.99, gender: 'm'})
   );
 };
