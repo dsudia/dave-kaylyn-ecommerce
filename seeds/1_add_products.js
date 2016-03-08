4bff05178377fe0e649c0913cb07b8055047d30e
David Sudia <dsudia@gmail.com>
2016-03-08 08:20:51 -0700

    fixed table name in add products seed file

--- seeds/1_add_products.js
+++ seeds/1_add_products.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     knex('products').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({name: 'Petrol Shoe', color: 'blue', image_file: 'images/womens-petrol-shoe-blue.png', price: 49.99, gender: 'f'}),
-    knex('table_name').insert({name: 'Petrol Boot', color: 'blue', image_file: 'images/womens-petrol-boot-blue.png', price: 69.99, gender: 'f'}),
-    knex('table_name').insert({name: 'Rouge Slipper', color: 'red', image_file: 'images/womens-slipper-red.png', price: 39.99, gender: 'f'}),
-    knex('table_name').insert({name: 'Rouge Boot', color: 'red', image_file: 'images/womens-boot-red.png', price: 69.99, gender: 'f'}),
-    knex('table_name').insert({name: 'Coal Boot', color: 'black', image_file: 'images/womens-boot-black.png', price: 69.99, gender: 'f'}),
-    knex('table_name').insert({name: 'Black Oxford', color: 'black', image_file: 'images/mens-oxfords-black.png', price: 99.99, gender: 'm'}),
-    knex('table_name').insert({name: 'Hip Sneaks', color: 'blue', image_file: 'images/mens-sneaker-blue.png', price: 39.99, gender: 'm'})
+    knex('products').insert({name: 'Petrol Shoe', color: 'blue', image_file: 'images/womens-petrol-shoe-blue.png', price: 49.99, gender: 'f'}),
+    knex('products').insert({name: 'Petrol Boot', color: 'blue', image_file: 'images/womens-petrol-boot-blue.png', price: 69.99, gender: 'f'}),
+    knex('products').insert({name: 'Rouge Slipper', color: 'red', image_file: 'images/womens-slipper-red.png', price: 39.99, gender: 'f'}),
+    knex('products').insert({name: 'Rouge Boot', color: 'red', image_file: 'images/womens-boot-red.png', price: 69.99, gender: 'f'}),
+    knex('products').insert({name: 'Coal Boot', color: 'black', image_file: 'images/womens-boot-black.png', price: 69.99, gender: 'f'}),
+    knex('products').insert({name: 'Black Oxford', color: 'black', image_file: 'images/mens-oxfords-black.png', price: 99.99, gender: 'm'}),
+    knex('products').insert({name: 'Hip Sneaks', color: 'blue', image_file: 'images/mens-sneaker-blue.png', price: 39.99, gender: 'm'})
   );
 };
