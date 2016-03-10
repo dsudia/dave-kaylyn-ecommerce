6af3a21f7a44212bcc8b814608764b19a0eafe78
David Sudia <dsudia@gmail.com>
2016-03-10 11:06:24 -0700

    added back-end route for stripe api

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -23,5 +23,23 @@ router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Express' });
 });
 
+router.post('/charge', function(req, res,next) {
+    var stripeToken = req.body.stripeToken;
+    var amount =  req.body.stripeAmount;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send('error');
+        } else {
+            res.send('success');
+        }
+    });
+});
+
 
 module.exports = router;
